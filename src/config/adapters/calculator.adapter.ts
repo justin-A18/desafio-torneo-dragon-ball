944ed8875f0f48011ee53145fdf9c4407553c17d
JKmoises <moi.prado@duocuc.cl>
2024-11-06 00:32:54 -0300

    feat: Agregar método para mezclar elementos de un array

--- src/config/adapters/calculator.adapter.ts
+++ src/config/adapters/calculator.adapter.ts
@@ -4,4 +4,14 @@ export class CalculatorADapter{
     // return Math.log2(n) % 1 === 0; // solución alternativa
     return (n & (n - 1)) === 0;
   }
+
+  static shuffleArray(array: any[]): any[] {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    return array;
+  }
 }
