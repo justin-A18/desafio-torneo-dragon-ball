114c05a74415258d6a5a96ac0ccb14015bd810c8
JKmoises <moi.prado@duocuc.cl>
2024-11-06 03:05:14 -0300

    feat: Crear lógica para decidir quien ataca primero en base al lucahdor con mayor velocidad

--- src/domain/use-cases/battle/start-battle.use-case.ts
+++ src/domain/use-cases/battle/start-battle.use-case.ts
@@ -1,15 +1,26 @@
 import { CharacterEntity } from "../../entities";
 
 interface StartBattleUseCase {
-  execute(fighter1: CharacterEntity,fighter2: CharacterEntity): CharacterEntity;
+  execute(
+    fighter1: CharacterEntity,
+    fighter2: CharacterEntity
+  ): CharacterEntity;
 }
 
 export class StartBattle implements StartBattleUseCase {
-
-  execute(fighter1: CharacterEntity,fighter2: CharacterEntity): CharacterEntity {
+  execute(
+    fighter1: CharacterEntity,
+    fighter2: CharacterEntity
+  ): CharacterEntity {
     let fighterHealth1 = 100;
     let fighterHealth2 = 100;
 
+    let [attacker, defender, attackerHealth, defenderHealth] =
+      fighter1.speed >= fighter2.speed
+        ? [fighter1, fighter2, fighterHealth1, fighterHealth2]
+        : [fighter2, fighter1, fighterHealth2, fighterHealth1];
+    
+    while (attackerHealth > 0 && defenderHealth > 0) { }
 
     return {} as CharacterEntity;
   }
